67d88decf4e14d296df23e5c1ef31f9c696cf5f7
Elver <elver.dioses18@gmail.com>
2026-06-02 10:49:58 -0500

    Agregar filtros historial

--- app/historial/page.tsx
+++ app/historial/page.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 
 export default function HistorialPage() {
   const [historial, setHistorial] = useState<any[]>([]);
+  const [filtroTema, setFiltroTema] = useState("Todos");
+const [ordenNota, setOrdenNota] = useState("reciente");
 
   useEffect(() => {
     const guardado = localStorage.getItem("historialExamenes");
@@ -13,6 +15,15 @@ export default function HistorialPage() {
       setHistorial(JSON.parse(guardado));
     }
   }, []);
+  const historialFiltrado = historial
+  .filter((item: any) =>
+    filtroTema === "Todos" ? true : item.tema === filtroTema
+  )
+  .sort((a: any, b: any) => {
+    if (ordenNota === "mayor") return b.nota - a.nota;
+    if (ordenNota === "menor") return a.nota - b.nota;
+    return 0;
+  });
 
   return (
     <main className="min-h-screen bg-slate-900 text-white p-6">
@@ -39,8 +50,34 @@ export default function HistorialPage() {
           </p>
         </div>
       ) : (
+        <>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 mb-6">
+  <select
+    value={filtroTema}
+    onChange={(e) => setFiltroTema(e.target.value)}
+    className="bg-slate-800 text-white border border-slate-600 rounded-xl p-3"
+  >
+    <option>Todos</option>
+    <option>Salud Pública</option>
+    <option>Gestión</option>
+    <option>Cuidado Integral</option>
+    <option>Ética</option>
+    <option>Investigación</option>
+    <option>Simulacro Mixto</option>
+  </select>
+
+  <select
+    value={ordenNota}
+    onChange={(e) => setOrdenNota(e.target.value)}
+    className="bg-slate-800 text-white border border-slate-600 rounded-xl p-3"
+  >
+    <option value="reciente">Más recientes</option>
+    <option value="mayor">Mayor nota a menor</option>
+    <option value="menor">Menor nota a mayor</option>
+  </select>
+</div>
         <div className="space-y-4">
-          {historial.map((item) => (
+          {historialFiltrado.map((item) => (
             <div
               key={item.id}
               className="bg-slate-800 rounded-2xl p-5 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
@@ -68,6 +105,7 @@ export default function HistorialPage() {
             </div>
           ))}
         </div>
+        </>
       )}
     </main>
   );
